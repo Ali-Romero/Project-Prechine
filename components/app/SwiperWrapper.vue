0e29eccd092d1daa33abc10248d855aebf4b81f0
17axah <17axah@mail.ru>
2022-11-13 15:38:27 +0600

    install deps

---
+++ components/app/SwiperWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="tag" class="swiper-wrapper">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.swiper-wrapper
+  position: relative
+  width: 100%
+  height: 100%
+  z-index: 1
+  display: flex
+  transition-property: transform
+  box-sizing: content-box
+</style>
